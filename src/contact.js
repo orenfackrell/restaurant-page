6f7713d46450b06a229d8686576b71d14c0d95fe
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-09-29 15:22:23 +0100

    add guild members to contact page

--- src/contact.js
+++ src/contact.js
@@ -1,27 +1,68 @@
+function randomNumber() {
+  for (let i = 0; i < 10; i++) {
+    let randomMobile = '07' + Math.random().toString().slice(2, 11);
+    return randomMobile;
+  }
+}
+
 const createContactPage = () => {
-    const contentDiv = document.getElementById('content');
-  
-    const contactDiv = document.createElement('div');
-    contactDiv.classList.add('contact');
-  
-    const contactTitle = document.createElement('h2');
-    contactTitle.textContent = 'Contact Us';
-    contactDiv.appendChild(contactTitle);
-  
-    const contactInfo = document.createElement('p');
-    contactInfo.textContent = 'For reservations or inquiries, please contact us at:';
-    contactDiv.appendChild(contactInfo);
-  
-    const contactEmail = document.createElement('p');
-    contactEmail.textContent = 'Email: info@restaurant.com';
-    contactDiv.appendChild(contactEmail);
-  
-    const contactPhone = document.createElement('p');
-    contactPhone.textContent = 'Phone: 123-456-7890';
-    contactDiv.appendChild(contactPhone);
-  
-    contentDiv.appendChild(contactDiv);
-  };
-  
-  export { createContactPage };
-  
+  const contentDiv = document.getElementById('content');
+
+  const people = [
+    {
+      name: 'Wigglytuff',
+      role: 'Guild Leader',
+      email: 'guildmaster@wigglytuffguild.pmd',
+      phone: randomNumber(),
+      icon: './src/images/wigglytuff-icon.png',
+    },
+    {
+      name: 'Chatot',
+      role: 'Guild Assistant',
+      email: 'guildassistant@wigglytuffguild.pmd',
+      phone: randomNumber(),
+      icon: './src/chatot-icon.png',
+    },
+    {
+      name: 'Dugtrio',
+      role: 'Security',
+      email: 'sentry@wigglytuffguild.pmd',
+      phone: randomNumber(),
+      icon: './src/dugtrio-icon.png',
+    },
+  ];
+
+  people.forEach((person) => {
+    const personDiv = document.createElement('div');
+    personDiv.classList.add('person');
+
+    const personName = document.createElement('h2');
+    personName.textContent = person.name;
+    personDiv.appendChild(personName);
+
+    const personRole = document.createElement('p');
+    personRole.textContent = person.role;
+    personDiv.appendChild(personRole);
+
+    const personIcon = document.createElement('img');
+    personIcon.src = person.icon;
+    personIcon.alt = `${person.name}'s icon`;
+    personDiv.appendChild(personIcon);
+
+    const contactList = document.createElement('ul');
+
+    const emailItem = document.createElement('li');
+    emailItem.textContent = `Email: ${person.email}`;
+    contactList.appendChild(emailItem);
+
+    const phoneItem = document.createElement('li');
+    phoneItem.textContent = `Phone: ${person.phone}`;
+    contactList.appendChild(phoneItem);
+
+    personDiv.appendChild(contactList);
+
+    contentDiv.appendChild(personDiv);
+  });
+};
+
+export { createContactPage };
