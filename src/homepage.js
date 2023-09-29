a4e1329037b57b4e4919fb2233843ff496a51084
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-09-29 14:57:40 +0100

    update homepage text

--- src/homepage.js
+++ src/homepage.js
@@ -1,19 +1,20 @@
 function createHomePage() {
-    const contentDiv = document.getElementById('content');
-  
-    const homepageDiv = document.createElement('div');
-    homepageDiv.classList.add('homepage');
-  
-    const headline = document.createElement('h1');
-    headline.textContent = 'Welcome to Our Restaurant!';
-  
-    const copy = document.createElement('p');
-    copy.textContent = 'Experience the finest dining in town. Our restaurant offers a wide range of delicious dishes prepared by our talented chefs. Come and enjoy a memorable dining experience with us.';
-  
-    homepageDiv.appendChild(headline);
-    homepageDiv.appendChild(copy);
-  
-    contentDiv.appendChild(homepageDiv);
-  }
-  
-  export { createHomePage }
+  const contentDiv = document.getElementById("content");
+
+  const homepageDiv = document.createElement("div");
+  homepageDiv.classList.add("homepage");
+
+  const headline = document.createElement("h2");
+  headline.textContent = "Welcome to The Pokémon Mystery Dungeon Café!";
+
+  const copy = document.createElement("p");
+  copy.textContent =
+    "Experience the finest dining in the world of PMD! Our café offers a selection of all the delicious food items found by our brave explorers and then prepared by our talented chefs. Come and enjoy a delicious snack to fill your hunger before beginning your next expedition, where ever that might take you!";
+
+  homepageDiv.appendChild(headline);
+  homepageDiv.appendChild(copy);
+
+  contentDiv.appendChild(homepageDiv);
+}
+
+export { createHomePage };
